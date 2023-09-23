ddd1d47b8e4e292be2d6f6c00752b7b582c3646e
unknown <kiarie7mbugua@gmail.com>
2023-09-23 22:40:00 +0300

    has bugs

--- Kevin.org.session.sql
+++ Kevin.org.session.sql
@@ -1,33 +1,44 @@
- INSERT INTO order_id (order_id, product_id, quantity) VALUES
-    (1, 1, 2),
-    (1, 2, 1),
-    (2, 2, 1),
-    (2, 3, 3),
-    (3, 1, 1),
-    (3, 3, 2),
-    (4, 2, 4),
-    (4, 3, 1),
-    (5, 1, 1),
-    (5, 3, 2),
-    (6, 2, 3),
-    (6, 1, 1),
-    (7, 4, 1),
-    (7, 5, 2),
-    (8, 6, 3),
-    (8, 7, 1),
-    (9, 8, 2),
-    (9, 9, 1),
-    (10, 10, 3),
-    (10, 11, 2),
-    (11, 12, 1),
-    (11, 13, 3),
-    (12, 4, 2),
-    (12, 5, 1),
-    (13, 6, 3),
-    (13, 7, 2),
-    (14, 8, 1),
-    (14, 9, 2),
-    (15, 10, 3),
-    (15, 11, 1),
-    (16, 12, 2),
-    (16, 13, 3);          
+
+-- This is a continuation of the ABC Database business problem. Here you will practice your querying skills. Your task is to answer the following two questions:
+
+-- Which product has the highest price? Only return a single row.
+-- Which order_id had the highest number of items in terms of quantity
+
+-- DESCRIBE customer;
+-- DESCRIBE order_id;
+-- DESCRIBE orders;
+-- DESCRIBE products;
+
+--  SELECT * FROM order_id;
+
+-- question 3
+-- SELECT * FROM products ORDER BY price DESC LIMIT 1;
+
+-- question 2
+-- SELECT * FROM order_id ORDER BY quantity DESC LIMIT 1;
+
+-- Which customer has made the most orders?
+
+-- SELECT order_count AS(
+--     SELECT customer_id COUNT(order_id) AS order_count
+--     FROM orders
+--     GROUP BY order_id,customer_id
+-- ),
+-- max_order_count AS(
+--     SELECT max(order_count) AS max_order_count
+--     FROM orders
+-- )
+-- SELECT first_name,last_name FROM
+-- customer
+
+-- JOIN order_count oc ON customer = oc.customer
+-- JOIN max_order_count moc ON oc.order_count = moc.max_order_count;
+
+-- what is the total revenue per product
+
+SELECT product_name,
+price * sum(quantity) AS total_revenue
+FROM products 
+JOIN order_id ON product_id = product_id
+GROUP BY quantity product_id,price,
+ORDER BY 2 DESC; 
